7fe41694b8cad81d7e7ce8e3413d63db02fcd6da
baoge <baoge_pmb@hotmail.com>
2021-07-30 16:09:56 +0800

    upgrade sdk to latest
    Former-commit-id: 1374979f10e9fd808ac2e75298b8f6283418c945 [formerly dab10345cd077a14ba02de8ab89e32b52001b38f]
    Former-commit-id: 63290dc42916f853dbd86676c3bff100650a5ea0

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import { NativeModules } from 'react-native';
 const { SdkBridge } = NativeModules;
 
 
-export class MisesSdk {
+export class MSdk {
   constructor(ptr) {
     this._ptr = ptr;
   }
 
-  static async newsdk() {
+  static async newSdk() {
     const ptr = await SdkBridge.newSdk();
-    return new MisesSdk(ptr);
+    return new MSdk(ptr);
   }
 
   testConnection() {
@@ -23,6 +23,12 @@ export class MisesSdk {
     return SdkBridge.sdkSetLogLevel(this._ptr, level);
   }
 
+  userMgr() {
+    return SdkBridge.sdkUserMgr(this._ptr);
+  }
+  randomMnemonics() {
+    return SdkBridge.sdkRandomMnemonics(this._ptr);
+  }
 }
 
 export default SdkBridge;
