9d8181d0e1a18373b6e410bdcd3715d3fb3a1e37
baoge <baoge_pmb@hotmail.com>
2021-08-03 16:23:21 +0800

    fix missing api stub
    Former-commit-id: b0bf1ed3d38b900f31ad2d6d70f817a743ed653b [formerly 0c5da39b62c152083a80a40eec4365a2371ae5c1]
    Former-commit-id: 72994be57e4ff724b4dde634df98da3a773c72d1

--- android/src/main/java/com/reactlibrary/SdkBridgeModule.java
+++ android/src/main/java/com/reactlibrary/SdkBridgeModule.java
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -35,13 +36,17 @@ public class SdkBridgeModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void newSdk(Promise promise) {
-        tryReact(promise, () ->
-                this.getPointer(Mobile.newMSdk())
-        );
+        tryReact(promise, () -> {
+            MSdk sdk = Mobile.newMSdk();
+            File dir = reactContext.getFilesDir();
+            sdk.setHomePath(dir.getAbsolutePath());
+            return this.getPointer(sdk);
+        });
     }
 
-    @ReactMethod
+    @ReactMethod()
     public void sdkTestConnection(String ptr, Promise promise) {
+
         tryReact(promise, () -> {
             MSdk msdk = this.getUnretainedObject(ptr);
             msdk.testConnection();
@@ -62,7 +67,7 @@ public class SdkBridgeModule extends ReactContextBaseJavaModule {
     public void sdkUserMgr(String ptr, Promise promise) {
         tryReact(promise, () -> {
             MSdk msdk = this.getUnretainedObject(ptr);
-            return msdk.userMgr();
+            return this.getPointer(msdk.userMgr());
         });
     }
     @ReactMethod
@@ -73,7 +78,182 @@ public class SdkBridgeModule extends ReactContextBaseJavaModule {
         });
     }
 
+    @ReactMethod
+    public void userMgrActiveUser(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserMgr muserMgr = this.getUnretainedObject(ptr);
+            MUser user = muserMgr.activeUser();
+            return this.getPointer(user);
+        });
+    }
+
+    @ReactMethod
+    public void userMgrCreateUser(String ptr, String mnemonic, String passPhrase, Promise promise) {
+        tryReact(promise, () -> {
+            MUserMgr muserMgr = this.getUnretainedObject(ptr);
+
+            MUser user = muserMgr.createUser(mnemonic, passPhrase);
+            return this.getPointer(user);
+        });
+    }
+
+    @ReactMethod
+    public void userMgrListUser(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserMgr muserMgr = this.getUnretainedObject(ptr);
+            MUserList userList = muserMgr.listUsers();
+
+            return this.getPointer(userList);
+        });
+    }
+
+    @ReactMethod
+    public void userMgrSetActiveUser(String ptr, String did,String pass,  Promise promise) {
+        tryReact(promise, () -> {
+            MUserMgr muserMgr = this.getUnretainedObject(ptr);
+            muserMgr.setActiveUser(did, pass);
+            return null;
+        });
+    }
+
+    @ReactMethod
+    public void userListCount(String ptr,Promise promise) {
+        tryReact(promise, () -> {
+            MUserList muserList = this.getUnretainedObject(ptr);
+            return muserList.count();
+        });
+    }
+    @ReactMethod
+    public void userListGet(String ptr, long idx, Promise promise) {
+        tryReact(promise, () -> {
+            MUserList muserList = this.getUnretainedObject(ptr);
+            return this.getPointer(muserList.get(idx));
+        });
+    }
+
+    @ReactMethod
+    public void stringListCount(String ptr,Promise promise) {
+        tryReact(promise, () -> {
+            MStringList stringList = this.getUnretainedObject(ptr);
+            return stringList.count();
+        });
+    }
+    @ReactMethod
+    public void stringListGet(String ptr, long idx, Promise promise) {
+        tryReact(promise, () -> {
+            MStringList stringList = this.getUnretainedObject(ptr);
+            return stringList.get(idx);
+        });
+    }
+
+    @ReactMethod
+    public void userMisesID(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            return muser.misesID();
+        });
+    }
+    @ReactMethod
+    public void userInfo(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            MUserInfo info = muser.info();
+            return this.getPointer(info);
+        });
+    }
+    @ReactMethod
+    public void userGetFollowing(String ptr, String appDid, Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            MStringList list = muser.getFollow(appDid);
+            return this.getPointer(list);
+        });
+    }
+
+    @ReactMethod
+    public void userSetFollow(String ptr, String did, boolean op, String appDid,Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            return  muser.setFollow(did, op, appDid);
+        });
+    }
+
+    @ReactMethod
+    public void userIsRegistered(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            return  muser.isRegistered();
+        });
+    }
+    @ReactMethod
+    public void userRegister(String ptr, String appDid,Promise promise) {
+        tryReact(promise, () -> {
+            MUser muser = this.getUnretainedObject(ptr);
+            muser.register(null, appDid);
+            return null;
+        });
+    }
+
+    @ReactMethod
+    public void userInfoName(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            return muserinfo.name();
+        });
+    }
+    @ReactMethod
+    public void userInfoGender(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            return muserinfo.gender();
+        });
+    }
+
+    @ReactMethod
+    public void userInfoAvatarDid(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            return muserinfo.avatarDid();
+        });
+    }
+
+    @ReactMethod
+    public void userInfoHomePage(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            return muserinfo.homePage();
+        });
+    }
+
+    @ReactMethod
+    public void userInfoEmails(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            MStringList list = muserinfo.emails();
+            return this.getPointer(list);
+        });
+    }
+    @ReactMethod
+    public void userInfoTelphones(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            MStringList list = muserinfo.telphones();
+            return this.getPointer(list);
+        });
+    }
+
+    @ReactMethod
+    public void userInfoIntro(String ptr, Promise promise) {
+        tryReact(promise, () -> {
+            MUserInfo muserinfo = this.getUnretainedObject(ptr);
+            return muserinfo.intro();
+        });
+    }
+
     private String getPointer(Object object) {
+        if (object == null) {
+            return null;
+        }
         long index = pointer.incrementAndGet();
         String ptr = Long.toHexString(index);
         pointers.put(ptr, object);
